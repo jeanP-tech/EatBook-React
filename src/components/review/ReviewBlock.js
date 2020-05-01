f17eee3da368a6fccbf27fe8f77ee072aae7b6f9
jeanP-tech <jeanP.tech@gmail.com>
2020-05-01 17:45:10 +0900

    Fix three_posts to flex-wrap in Review.css

--- src/components/review/ReviewBlock.js
+++ src/components/review/ReviewBlock.js
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import '../../css/Review.css';
 
-const Block = () => {
-  const reviews = [...Array(3).keys()].map(i  => ({
-    id: i,
-    img: 'http://the-edit.co.kr/wp-content/uploads/2018/07/batch_IMG_4930.jpg',
-    title: '<목소리를 드릴게요>를 읽고 적는 감상평',
-  }));
+class Block extends Component {
 
-  const ReviewList = reviews.map((review) => (
-    <div key={review['id']} className="post">
-      <Link to={`/review/목소리를 드릴게요`}>
-        <img src={review['img']} />
-      </Link>
-      <Link to={`/review/목소리를 드릴게요`}>
-        <h4 className="title">{review['title']}</h4>
-      </Link>
-    </div>
-  ));
+  render(){
+    const reviews = [...Array(3).keys()].map(i  => ({
+      id: i,
+      img: 'http://the-edit.co.kr/wp-content/uploads/2018/07/batch_IMG_4930.jpg',
+      title: '<목소리를 드릴게요>를 읽고 적는 감상평',
+    }));
 
-  return (
-    <>
-    <div className="review_block_container">
-      <div className="review_block">
-        <h3>재미있게 읽은 #목소리를드릴게요 의 다른 감상들</h3>
-        <div className="three_posts">
-          { ReviewList }
+    const ReviewList = reviews.map((review) => (
+      <div key={review['id']} className="post">
+        <Link to={`/review/목소리를 드릴게요`}>
+          <img src={review['img']} />
+        </Link>
+        <Link to={`/review/목소리를 드릴게요`}>
+          <h4 className="title">{review['title']}</h4>
+        </Link>
+      </div>
+    ));
+
+    return (
+      <>
+      <div className="review_block_container">
+        <div className="review_block">
+          <h3>재미있게 읽은 #목소리를드릴게요 의 다른 감상들</h3>
+          <div className="three_posts">
+            { ReviewList }
+          </div>
         </div>
       </div>
-    </div>
-    </>
-  );
+      </>
+    );
+  }
+
 };
 
 export default Block;
